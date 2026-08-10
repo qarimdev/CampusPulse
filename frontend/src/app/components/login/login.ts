c3f79e3dfaf8552e6d361fcc2c5751f9eacf7423
qarimdev <qarimdev@gmail.com>
2026-08-10 17:17:30 +0800

    feat(frontend): create login UI component and route integration

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,9 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
+
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  onLogin() {
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.credentials).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        console.log('Login successful', res);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Invalid email or password.';
+      },
+    });
+  }
+}
